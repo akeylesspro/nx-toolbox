45269dce37cb3460f08c812e71642032662da5a2
avrhahm sason <avi6190i@gmail.com>
2024-12-15 18:45:21 +0200

    .

--- src/app/(main-router)/reports/not-active-cars/NotActiveDevices.tsx
+++ src/app/(main-router)/reports/not-active-cars/NotActiveDevices.tsx
@@ -2,8 +2,12 @@
 import { TObject } from "akeyless-types-commons";
 import React, { useEffect } from "react";
 import { NotActiveTable } from "./components";
+import { useTranslation } from "react-i18next";
+import { useDocumentTitle } from "akeyless-client-commons/hooks";
 
 function NotActiveDevices({ data }: { data: TObject<any>[] }) {
+    const { t } = useTranslation();
+    useDocumentTitle(t("not_active_cars"));
     return <NotActiveTable data={data} />;
 }
 

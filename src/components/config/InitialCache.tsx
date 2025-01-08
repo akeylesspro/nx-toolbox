747944e645f38ea795c4fbb1ad7ed6c5a6953625
avrhahm sason <avi6190i@gmail.com>
2025-01-08 11:22:49 +0200

    .

--- src/components/config/InitialCache.tsx
+++ src/components/config/InitialCache.tsx
@@ -29,6 +29,7 @@ export default function InitialCache() {
                     setBoards((prev) => {
                         return [...prev, ...data];
                     });
+                    setBoardTypes((prev) => (prev.includes(data[0].type) ? prev : [...prev, data[0].type]));
                 },
                 onModify: (data) => {
                     setBoards((prev) => {
